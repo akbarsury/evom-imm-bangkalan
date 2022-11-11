ee3e721778aaf26f1034721386910071ce098dc2
akbarsury <190411100119@student.trunojoyo.ac.id>
2022-11-12 02:25:44 +0700

    reupload

--- file/kandidat-layout.2b79f2d0.js
+++ file/kandidat-layout.2b79f2d0.js
@@ -1,1 +1,1 @@
-var a="https://akbarsury.github.io/evom-imm-bangkalan/file/kandidat-layout.d0816525.png";export{a as _};
+var a="/evom-imm-bangkalan/file/kandidat-layout.d0816525.png";export{a as _};
